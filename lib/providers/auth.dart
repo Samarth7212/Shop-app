963e86d1bc4eab4d9c688504c792e326aac23941
Samarth7212 <samarthkamble7212@gmail.com>
2022-01-16 11:47:56 +0530

    Authentication working correctly. Error handling added for sign in and sign up procedures.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,29 +1,49 @@
+// ignore_for_file: unused_local_variable, unused_field
+
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:newshop/models/http_exceptions.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
+//We need return/await here to wait till the function is executed
   Future<void> signup(String email, String password) async {
-    print('Inside auth.dart');
-    // final url = Uri.parse(
-    //     'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyBV8rYGykrUHFfIgjTMGvR0wbavfbSN8Mw');
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> signin(String email, String password) async {
+    await _authenticate(email, password, 'signInWithPassword');
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBV8rYGykrUHFfIgjTMGvR0wbavfbSN8Mw');
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBV8rYGykrUHFfIgjTMGvR0wbavfbSN8Mw');
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseata = jsonDecode(response.body);
+      if (responseata['error'] != null) {
+        throw HttpException(responseata['error']['message']);
+      }
+      // print(jsonDecode(response.body));
+      return response;
+    } on Exception catch (_) {
+      rethrow;
+    }
   }
 }
